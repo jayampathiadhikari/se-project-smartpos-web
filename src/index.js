2f4a479c24107232766d0011b5338a0f9eb03641
SachiniAbeygunawardhana <47135264+SachiniAbeygunawardhana@users.noreply.github.com>
2020-04-23 08:08:51 +0530

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -18,30 +18,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
-import { Provider } from 'react-redux'
-import {store,persistor} from './redux/configureStore';
-import { PersistGate } from 'redux-persist/es/integration/react';
+
 import "assets/plugins/nucleo/css/nucleo.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "assets/scss/argon-dashboard-react.scss";
 
-import requiresAuth from './components/RequiresAuth';
 import AdminLayout from "layouts/Admin.js";
 import AuthLayout from "layouts/Auth.js";
-
 ReactDOM.render(
-  <Provider store={store}>
-    <PersistGate persistor={persistor}>
-    <BrowserRouter>
-      <Switch>
-        <Route path="/admin" component={requiresAuth(AdminLayout)} />
-        <Route path="/auth" render={props => <AuthLayout {...props} />} />
-        <Redirect from="/" to="/admin/index" />
-      </Switch>
-    </BrowserRouter>
-    </PersistGate>
-  </Provider>
-  ,
-
+  <BrowserRouter>
+    <Switch>
+      <Route path="/admin" render={props => <AdminLayout {...props} />} />
+      <Route path="/auth" render={props => <AuthLayout {...props} />} />
+      <Redirect from="/" to="/admin/index" />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById("root")
 );
