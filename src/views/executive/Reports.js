2acaa011bdb3340ecc6f4397a729a2fe74f566b5
jayampathiadhikari <jayampathiadhikari@gmail.com>
2020-05-29 04:41:00 +0530

    exec reports

--- src/views/executive/Reports.js
+++ src/views/executive/Reports.js
@@ -7,7 +7,18 @@ import {
   CardFooter,
   Container,
   Row,
-  Col, Table, Media, UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem, Nav, NavItem, NavLink, Button
+  Col,
+  Table,
+  Media,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  Nav,
+  NavItem,
+  NavLink,
+  Button,
+  Input
 } from "reactstrap";
 // core components
 
@@ -15,6 +26,7 @@ import HeaderNoCards from "../../components/Headers/HeaderNoCards";
 import Pagination from "react-js-pagination";
 import Executive from "../../models/Executive";
 import classnames from "classnames";
+import {connect} from "react-redux";
 
 //id,name,prod cost, selling price,, quantity
 
@@ -23,15 +35,19 @@ class ExecReports extends React.Component {
     agent_id: null,
     agentData: [],
     pageSize: 5,
-    activePage:1,
-    activeButton : '1',
-    productsByMonth : [],
-    productsByYear : [],
-    districtsByMonth : [],
-    districtsByYear: []
+    activePage: 1,
+    activeButton: '1',
+    activeButtonDistrict: '1',
+    activeButtonProduct: '1',
+    productsByMonth: [],
+    productsByYear: [],
+    products: 'productsByMonth',
+    districtsByMonth: [],
+    districtsByYear: [],
+    districts: 'districtsByMonth'
   };
 
-  componentDidMount = async() =>{
+  componentDidMount = async () => {
     const res = await Executive.getAllAgents()
     this.setState({
       agentData: res
@@ -42,14 +58,14 @@ class ExecReports extends React.Component {
   seeReports = (agent_id) => {
     this.props.history.push({
       pathname: '/executive/reports/view-reports',
-      state: {agent_id:agent_id}
+      state: {agent_id: agent_id}
     })
   };
 
   renderTableRows = () => {
     const {pageSize, activePage, agentData} = this.state;
-    const pagedArray = agentData.slice(pageSize*(activePage-1),pageSize*activePage);
-    return pagedArray.map((item,index) => (
+    const pagedArray = agentData.slice(pageSize * (activePage - 1), pageSize * activePage);
+    return pagedArray.map((item, index) => (
         <tr key={index.toString()}>
           <th scope="row">
             <Media className="align-items-center">
@@ -76,7 +92,10 @@ class ExecReports extends React.Component {
               <DropdownMenu className="dropdown-menu-arrow" right>
                 <DropdownItem
                   href="#pablo"
-                  onClick={e => {e.preventDefault(); this.seeReports(item.uid)}}
+                  onClick={e => {
+                    e.preventDefault();
+                    this.seeReports(item.uid)
+                  }}
                 >
                   See Reports
                 </DropdownItem>
@@ -93,46 +112,161 @@ class ExecReports extends React.Component {
     this.setState({activePage: pageNumber});
   }
 
-  onClick = async(e) => {
+  onClick = async (e) => {
     const button = e.target.value;
     this.setState({
-      activeButton : button
+      activeButton: button
     });
-    if(button=== '2' && this.state.productsByMonth.length === 0){
-      const productsByMonth = await Executive.getTopProductsByMonth();
-      const productsByYear = await Executive.getTopProductsByYear();
-      if(productsByYear.success && productsByMonth.success){
+    if (button === '2' && this.state.productsByMonth.length === 0) {
+      const productsByMonth = await Executive.getTopProductsByMonth(this.props.user.uid);
+      const productsByYear = await Executive.getTopProductsByYear(this.props.user.uid);
+      if (productsByYear.success && productsByMonth.success) {
         this.setState({
           productsByMonth: productsByMonth.data,
           productsByYear: productsByYear.data
         })
       }
-      console.log(productsByMonth,productsByYear,'data first')
-    }else if (button === '3' && this.state.districtsByMonth.length === 0){
-      const districtsByMonth = await Executive.getTopProductsByMonth();
-      const districtsByYear = await Executive.getTopProductsByYear();
-      if(districtsByMonth.success && districtsByYear.success){
+      console.log(productsByMonth, productsByYear, 'data first')
+    } else if (button === '3' && this.state.districtsByMonth.length === 0) {
+      const districtsByMonth = await Executive.getTopDistrictsByMonth();
+      const districtsByYear = await Executive.getTopDistrictsByYear();
+      if (districtsByMonth.success && districtsByYear.success) {
         this.setState({
           districtsByMonth: districtsByMonth.data,
           districtsByYear: districtsByYear.data
         })
       }
-      console.log(districtsByMonth,districtsByYear,'data first d')
+      console.log(districtsByMonth, districtsByYear, 'data first d')
     }
 
   };
 
+  onClickProduct = (e) => {
+    const button = e.target.value;
+    this.setState({
+      activeButtonProduct : button,
+      products :
+        button === '1' ? 'productsByMonth' : 'productsByYear'
+    })
+  };
 
   renderTopSellingProducts = () => {
-    return null
+    return (
+      <Row className='mt-7'>
+        <Col className="order-xl-2 mb-5 mb-xl-0" xl="4">
+        </Col>
+        <div className="col">
+          <Card className="shadow">
+            <CardHeader className="border-0">
+              <Row>
+                <Col lg={5}>
+                  <h3 className="mb-0">Reports</h3>
+                </Col>
+                <Col lg={7}>
+                  <div className="pagination justify-content-end mb-0">
+                    <span>
+                      <Button size={'md'} value={'1'} color={this.state.activeButtonProduct === '1' ? 'primary' : 'secondary'}
+                              onClick={this.onClickProduct}>Month</Button>
+                      <Button size={'md'} value={'2'} color={this.state.activeButtonProduct === '2' ? 'primary' : 'secondary'}
+                              onClick={this.onClickProduct}>Year</Button>
+                    </span>
+                  </div>
+                </Col>
+              </Row>
+            </CardHeader>
+            <Table className="align-items-center table-flush" responsive>
+              <thead className="thead-light">
+              <tr>
+                <th scope="col">Product</th>
+                <th scope="col">Revenue</th>
+              </tr>
+              </thead>
+              <tbody>
+              {this.state[this.state.products].map((item, index) => (
+                <tr key={index.toString()}>
+                  <td>{item.product_name}</td>
+                  <td>
+                    {item.total_revenue}
+                  </td>
+                </tr>
+              ))}
+              </tbody>
+            </Table>
+            <CardFooter className="py-4">
+              <div className="pagination justify-content-end mb-0">
+              </div>
+            </CardFooter>
+          </Card>
+        </div>
+      </Row>
+    )
+  };
+
+  onClickDistrict = (e) => {
+    const button = e.target.value;
+    this.setState({
+      activeButtonDistrict : button,
+      districts :
+        button === '1' ? 'districtsByMonth' : 'districtsByYear'
+    })
   };
 
   renderTopSellingDistricts = () => {
-    return null
+    return (
+
+      <Row className='mt-7'>
+        <Col className="order-xl-2 mb-5 mb-xl-0" xl="4">
+        </Col>
+        <div className="col">
+          <Card className="shadow">
+            <CardHeader className="border-0">
+              <Row>
+                <Col lg={5}>
+                  <h3 className="mb-0">Reports</h3>
+                </Col>
+                <Col lg={7}>
+                  <div className="pagination justify-content-end mb-0">
+                    <span>
+                      <Button size={'md'} value={'1'} color={this.state.activeButtonDistrict === '1' ? 'primary' : 'secondary'}
+                              onClick={this.onClickDistrict}>Month</Button>
+                      <Button size={'md'} value={'2'} color={this.state.activeButtonDistrict === '2' ? 'primary' : 'secondary'}
+                              onClick={this.onClickDistrict}>Year</Button>
+                    </span>
+                  </div>
+                </Col>
+              </Row>
+
+            </CardHeader>
+            <Table className="align-items-center table-flush" responsive>
+              <thead className="thead-light">
+              <tr>
+                <th scope="col">District</th>
+                <th scope="col">Revenue</th>
+              </tr>
+              </thead>
+              <tbody>
+              {this.state[this.state.districts].map((item, index) => (
+                <tr key={index.toString()}>
+                  <td>{item.district_name}</td>
+                  <td>
+                    {item.total_revenue}
+                  </td>
+                </tr>
+              ))}
+              </tbody>
+            </Table>
+            <CardFooter className="py-4">
+              <div className="pagination justify-content-end mb-0">
+              </div>
+            </CardFooter>
+          </Card>
+        </div>
+      </Row>
+    )
   };
 
   renderAgentwiseSales = () => {
-    return(
+    return (
       <Row className='mt-7'>
         <Col className="order-xl-2 mb-5 mb-xl-0" xl="4">
         </Col>
@@ -143,7 +277,7 @@ class ExecReports extends React.Component {
             </CardHeader>
             <Table className="align-items-center table-flush" responsive>
               <thead className="thead-light">
-              <tr >
+              <tr>
                 <th scope="col">Name</th>
                 <th scope="col">District</th>
                 <th scope="col"/>
@@ -173,16 +307,16 @@ class ExecReports extends React.Component {
   };
 
   renderContent = () => {
-    if (this.state.activeButton === '1'){
+    if (this.state.activeButton === '1') {
       return this.renderAgentwiseSales()
-    }
-    else if (this.state.activeButton === '2'){
+    } else if (this.state.activeButton === '2') {
       return this.renderTopSellingProducts()
 
-    }else{
+    } else {
       return this.renderTopSellingDistricts()
     }
   };
+
   render() {
     return (
       <>
@@ -192,9 +326,12 @@ class ExecReports extends React.Component {
           <Row>
             <div className="col">
               <span>
-                <Button size={'lg'} value={'1'} color={this.state.activeButton === '1' ? 'primary' : 'secondary'} onClick={this.onClick}>Agentwise Sales</Button>
-                <Button size={'lg'} value={'2'} color={this.state.activeButton === '2' ? 'primary' : 'secondary'} onClick={this.onClick}>Top Selling Products</Button>
-                <Button size={'lg'} value={'3'} color={this.state.activeButton === '3' ? 'primary' : 'secondary'} onClick={this.onClick}>Top Selling Districts</Button>
+                <Button size={'lg'} value={'1'} color={this.state.activeButton === '1' ? 'primary' : 'secondary'}
+                        onClick={this.onClick}>Agentwise Sales</Button>
+                <Button size={'lg'} value={'2'} color={this.state.activeButton === '2' ? 'primary' : 'secondary'}
+                        onClick={this.onClick}>Top Selling Products</Button>
+                <Button size={'lg'} value={'3'} color={this.state.activeButton === '3' ? 'primary' : 'secondary'}
+                        onClick={this.onClick}>Top Selling Districts</Button>
             </span>
             </div>
           </Row>
@@ -206,4 +343,14 @@ class ExecReports extends React.Component {
   }
 }
 
-export default ExecReports;
+const mapStateToProps = (state) => ({
+  user: state.AuthenticationReducer.user,
+});
+
+const bindAction = (dispatch) => ({});
+
+export default connect(
+  mapStateToProps,
+  bindAction
+)(ExecReports);
+
