a8c78a1cdb801fb8043d2965689218f2c62f2d48
jayampathiadhikari <jayampathiadhikari@gmail.com>
2020-06-02 17:05:29 +0530

    productwise reports

--- src/views/executive/Reports - Productwise.js
+++ src/views/executive/Reports - Productwise.js
@@ -34,6 +34,7 @@ import {
   pieGraph
 } from "variables/charts.js";
 import Chart from "chart.js";
+import CustomDropdown from "../../components/Dropdown";
 
 //id,name,prod cost, selling price,, quantity
 
@@ -50,7 +51,9 @@ class ReportsProductwise extends React.Component {
     processing:true,
     msg:null,
 
+    stock:[],
     activeNav: 1,
+    activeNavBar: 1,
     horizontalGraphData:'bestSellingProductsMonth',
     agent_id: null,
     pageSize: 10,
@@ -58,7 +61,7 @@ class ReportsProductwise extends React.Component {
     productsByMonth: [],
     productsByYear: [],
     products: 'productsByMonth',
-
+    chartExample2Data: "data3",
   };
 
   componentDidMount = async () => {
@@ -69,16 +72,25 @@ class ReportsProductwise extends React.Component {
       console.log(productsByMonth);
       horizontalGraph.bestSellingProductsMonth = (canvas) => (getGraphDataProducts(productsByMonth.data));
       horizontalGraph.bestSellingProductsYear = (canvas) => (getGraphDataProducts(productsByYear.data));
-
       this.setState({
         productsByMonth: productsByMonth.data,
         productsByYear: productsByYear.data,
         visible:false
       })
     }
-    if (window.Chart) {
-      parseOptions(Chart, chartOptions());
+    const res = await Executive.getStock();
+    const stock = [];
+    if (res.success) {
+      res.data.forEach((data) => {
+        stock.push({
+          name: data.name,
+          id: data.product_id
+        })
+      })
     }
+    this.setState({
+      stock
+    })
   };
 
   handlePageChange(pageNumber) {
@@ -92,6 +104,15 @@ class ReportsProductwise extends React.Component {
       activeNav: index,
       horizontalGraphData:
         this.state.horizontalGraphData === "bestSellingProductsMonth" ? "bestSellingProductsYear" : "bestSellingProductsMonth",
+    });
+  };
+
+  toggleNavsBar = (e, index) => {
+    e.preventDefault();
+    this.setState({
+      activeNavBar: index,
+      chartExample2Data:
+        this.state.chartExample2Data === "data3" ? "data4" : "data3",
     });
   };
 
@@ -115,7 +136,80 @@ class ReportsProductwise extends React.Component {
     })
   };
 
+  onSelect = async (product) => {
+    const barData = await Executive.getBarGraphData(this.props.user.uid, product.id);
+    const barDataDistrictMonthly = await Executive.getDistrictMonthBarData(product.id);
+    const barDataDistrictAnnually = await Executive.getDistrictYearBarData(product.id);
+
+    chartExample2.data3 = (canvas) => (barDataDistrictMonthly);
+    chartExample2.data4 = (canvas) => (barDataDistrictAnnually);
+    this.setState({
+      productName: product.name,
+      productID: product.id,
+      barData: barData
+    })
+  };
   renderBarGraph = (data) => {
+    return (
+      <Col className="xl-12">
+        <Card className="shadow">
+          <CardHeader className="bg-transparent">
+            <Row className="align-items-center">
+              <div className="col">
+                <h6 className="text-uppercase text-muted ls-1 mb-1">
+                  District wise Performance
+                </h6>
+                {this.state.productID ?
+                  <h2 className="mb-0">Total orders of {this.state.productName}</h2> :
+                  <h2 className="mb-0">No product selected</h2>
+                }
+              </div>
+              <div className="col">
+                <Nav className="justify-content-end" pills>
+                  <NavItem>
+                    <NavLink
+                      className={classnames("py-2 px-3", {
+                        active: this.state.activeNavBar === 1
+                      })}
+                      href="#pablo"
+                      onClick={e => this.toggleNavsBar(e, 1)}
+                    >
+                      <span className="d-none d-md-block">Month</span>
+                      <span className="d-md-none">M</span>
+                    </NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink
+                      className={classnames("py-2 px-3", {
+                        active: this.state.activeNavBar === 2
+                      })}
+                      data-toggle="tab"
+                      href="#pablo"
+                      onClick={e => this.toggleNavsBar(e, 2)}
+                    >
+                      <span className="d-none d-md-block">Year</span>
+                      <span className="d-md-none">W</span>
+                    </NavLink>
+                  </NavItem>
+                </Nav>
+              </div>
+            </Row>
+          </CardHeader>
+          <CardBody>
+            {/* Chart */}
+            <div className="chart">
+              <Bar
+                data={data}
+                options={chartExample2.options}
+                getDatasetAtEvent={e => console.log(e)}
+              />
+            </div>
+          </CardBody>
+        </Card>
+      </Col>
+    )
+  };
+  renderHorizontalBarGraph = (data) => {
     return (
       <Col className="mb-5 mb-xl-0" xl="12">
         <Card className="bg-gradient-default shadow">
@@ -123,13 +217,15 @@ class ReportsProductwise extends React.Component {
             <Row className="align-items-center">
               <div className="col">
                 <h6 className="text-uppercase text-light ls-1 mb-1">
-                  Top Trending
+                  Top Trending Products
                 </h6>
-                <h2 className="text-white mb-0">Products</h2>
+                <h2 className="text-white mb-0">Based on the sales from last {this.state.activeNav === 1 ? 'month' : 'year'}</h2>
               </div>
               <div className="col">
                 <Nav className="justify-content-end" pills>
+
                 <NavItem>
+
                   <NavLink
                     className={classnames("py-2 px-3", {
                       active: this.state.activeNav === 1
@@ -164,39 +260,6 @@ class ReportsProductwise extends React.Component {
               <HorizontalBar
                 data={data}
                 options={horizontalGraph.options}
-                getDatasetAtEvent={e => console.log(e)}
-              />
-            </div>
-          </CardBody>
-        </Card>
-      </Col>
-    )
-  };
-
-  renderDoughnut = (data) => {
-    return (
-      <Col className="mb-5 mb-xl-0" xl="12">
-        <Card className="bg-gradient-default shadow">
-          <CardHeader className="bg-transparent">
-            <Row className="align-items-center">
-              <div className="col">
-                <h6 className="text-uppercase text-light ls-1 mb-1">
-                  Top Trending
-                </h6>
-                <h2 className="text-white mb-0">Products</h2>
-              </div>
-              <div className="col">
-                <Nav className="justify-content-end" pills>
-                </Nav>
-              </div>
-            </Row>
-          </CardHeader>
-          <CardBody>
-            {/* Chart */}
-            <div className="chart">
-              <Pie
-                data={data}
-                options={pieGraph.options}
                 getDatasetAtEvent={e => console.log(e)}
               />
             </div>
@@ -259,9 +322,6 @@ class ReportsProductwise extends React.Component {
   };
 
 
-  renderContent = () => {
-  };
-
   render() {
     return (
       <>
@@ -286,9 +346,9 @@ class ReportsProductwise extends React.Component {
           </Row>
           {/* Table */}
           <Row className={'mt-7'}>
-            {this.renderBarGraph(horizontalGraph[this.state.horizontalGraphData])}
+            {this.renderHorizontalBarGraph(horizontalGraph[this.state.horizontalGraphData])}
           </Row>
-
+          {/*pie graph*/}
           <Row className={'mt-5'}>
             <Col className="mb-5 mb-xl-0" xl="12">
               <Card className="bg-gradient-default shadow">
@@ -319,6 +379,74 @@ class ReportsProductwise extends React.Component {
               </Card>
             </Col>
           </Row>
+
+        {/*  single product */}
+        <Row className={'mt-5'}>
+          <Col className="mb-5 mb-xl-0" xl="12">
+            <Card className="bg-gradient-default shadow">
+              <CardHeader className="bg-transparent">
+                <Row className="align-items-center">
+                  <div className="col">
+                    <h6 className="text-uppercase text-light ls-1 mb-1">
+                      Top Trending
+                    </h6>
+                    <h2 className="text-white mb-0">Products</h2>
+                  </div>
+                  <div className="col">
+                    <Nav className="justify-content-end" pills>
+                    </Nav>
+                  </div>
+                </Row>
+              </CardHeader>
+              <CardBody>
+                {/* dropdown */}
+                <Row className="mt-5">
+                  <Col xl="4">
+                    <CustomDropdown data={this.state.stock} initial="Select a product"
+                                    disabled={this.state.stock.length === 0} onSelect={this.onSelect}/>
+                  </Col>
+                </Row>
+                {/* Chart 1 */}
+
+                {/*bar 1*/}
+                <Row className="mt-5">
+                  {
+                    <Col xl="12">
+                      <Card className="shadow">
+                        <CardHeader className="bg-transparent">
+                          <Row className="align-items-center">
+                            <div className="col">
+                              <h6 className="text-uppercase text-muted ls-1 mb-1">
+                                Annual Performance
+                              </h6>
+                              {this.state.productID ?
+                                <h2 className="mb-0">Total orders of {this.state.productName}</h2> :
+                                <h2 className="mb-0">No product selected</h2>
+                              }
+                            </div>
+                          </Row>
+                        </CardHeader>
+                        <CardBody>
+                          {/* Chart */}
+                          <div className="chart">
+                            <Bar
+                              data={this.state.barData}
+                              options={chartExample2.options}
+                            />
+                          </div>
+                        </CardBody>
+                      </Card>
+                    </Col>
+                  }
+                </Row>
+              {/*  bar 2*/}
+                <Row className="mt-5">
+                  {this.renderBarGraph(chartExample2[this.state.chartExample2Data])}
+                </Row>
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
 
         </Container>
       </>
